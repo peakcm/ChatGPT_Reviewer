29c7169db531f440d64b07325b2d074869be6d22
Corey Peak <peak@mail.harvard.edu>
2023-06-08 16:38:04 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        concept_note = request.form['concept_note']
+        review_type = request.form['review_type']
+        
+        # Generate response from ChatGPT
+        chatgpt_output = generate_chatgpt_response(concept_note, review_type)
+        
+        return render_template('results.html', chatgpt_output=chatgpt_output)
+    
+    return render_template('index.html')
+
+# This function is just a placeholder. You should replace this with the actual ChatGPT call.
+def generate_chatgpt_response(concept_note, review_type):
+    return f"ChatGPT response for concept_note: {concept_note} with review_type: {review_type}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
